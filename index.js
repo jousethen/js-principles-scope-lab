8ee12b0d857a72cff7800ec0a014cecb74a3d2a5
jousethen <jthen93@gmail.com>
2021-08-05 16:44:47 -0400

    Passing Tests

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "Josie";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(newName) {
+  bestCustomer = newName;
+}
+
+function changeLeastFavoriteCustomer(newName) {
+  leastFavoriteCustomer = newName;
+}
